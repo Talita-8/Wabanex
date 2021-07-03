846c30783cf9b4e4a6281f03b8771d498ad2b837
Talita-8 <talita.silva@pravaler.com.br>
2021-07-02 22:12:16 -0300

    feat: Implementa graphql

---
+++ lib/wabanex_web/schemas/types/user.ex
@@ -0,0 +1,10 @@
+defmodule Wabanex.Schemas.Types.User do
+  use Absinthe.Schema.Notation
+
+  @desc "Logic user representation"
+  object :user do
+    field :id, non_null(:uuid4)
+    field :name, non_null(:string)
+    field :email, non_null(:string)
+  end
+end
